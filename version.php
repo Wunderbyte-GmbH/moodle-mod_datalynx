f5029d6fc1489a1e83c743a726311bcfcb1fac99
David Bogner <davidbogner@gmail.com>
2015-10-06 07:13:52 +0200

    textarea rendering updated

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
  */
 
 $plugin->component = 'mod_datalynx';
-$plugin->version   = 2015091700;
+$plugin->version   = 2015092200;
 $plugin->release   = 'Skyler White 1.1'; //famous movie, theater and tv series characters
 $plugin->requires  = 2014050800;
 $plugin->maturity  = MATURITY_STABLE;
