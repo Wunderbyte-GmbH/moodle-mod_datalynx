f25378486570e2e74bb7bfe709065a57ace2e362
michaelpollak <github@michaelpollak.org>
2018-05-08 13:33:01 +0200

    bug#1042 Working on the backup structure for all databases.

--- backup/moodle2/backup_datalynx_stepslib.php
+++ backup/moodle2/backup_datalynx_stepslib.php
@@ -164,7 +164,10 @@ class backup_datalynx_activity_structure_step extends backup_activity_structure_
 
         // TODO: fix sql, this is just a temporary fix and does not provide same functionality for postgresql.
         // SQL for mysql provides id mapping of the field datalynx view, whereas there is no id mapping for postgresql.
-        // if ($CFG->dbtype == 'mysqli' || $CFG->dbtype == 'mysql') {
+        // Possible DBs: 'pgsql', 'mariadb', 'mysqli', 'mssql', 'sqlsrv' or 'oci'
+        // The cases are weird, they are formated differently in mssql, postgresql, mysql, ... fix that.
+        /* SELECT *, case  when rev=1 then 'blabla' end FROM docs works on mysql, mssql, postgresql */
+        if ($CFG->dbtype == 'mysqli' || $CFG->dbtype == 'mysql' || $CFG->dbtype == 'mariadb') {
             $field->set_source_sql(
                     "SELECT f.*,
                         CASE f.type WHEN 'datalynxview' THEN MAX(c.fullname) ELSE NULL END AS targetcourse,
@@ -184,8 +187,9 @@ class backup_datalynx_activity_structure_step extends backup_activity_structure_
                   WHERE f.dataid = :dataid
                GROUP BY f.id",
                     array('dataid' => backup::VAR_PARENTID));
-        /*
-        } else {
+        } else if ($CFG->dbtype == 'pgsql') {
+        }
+        else {
             $field->set_source_sql(
                     "SELECT f.*
                FROM {datalynx_fields} f
@@ -193,7 +197,6 @@ class backup_datalynx_activity_structure_step extends backup_activity_structure_
                 AND f.type != 'datalynxview'",
                     array('dataid' => backup::VAR_PARENTID));
         }
-        */
 
         $filter->set_source_table('datalynx_filters', array('dataid' => backup::VAR_PARENTID));
         $view->set_source_table('datalynx_views', array('dataid' => backup::VAR_PARENTID));
