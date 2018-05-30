66bb6a2189146b9bf431bf65456230dad16718e8
David Bogner <davidbogner@gmail.com>
2018-05-30 20:56:00 +0200

    Fix undefined variable

--- field/teammemberselect/renderer.php
+++ field/teammemberselect/renderer.php
@@ -238,8 +238,8 @@ class datalynxfield_teammemberselect_renderer extends datalynxfield_renderer {
                 }
             }
             if (isset($formdata->$formfieldname)) {
-                // Get rid of Dummy value -999 to correct calculations.
-                if ($formdata->{$formfieldname[0]} == -999) {
+                // Get rid of Dummy value -999 to correct calculations. TODO: Check still necessary?
+                if ($formdata->{$formfieldname}[0] == -999) {
                     array_shift($formdata->{$formfieldname});
                 }
                 // Limit chosen users to max teamsize and ensure min teamsize users are chosen!
