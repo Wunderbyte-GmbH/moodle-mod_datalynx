08a84e6be313bff6510428b05244493f8e579685
michaelpollak <github@michaelpollak.org>
2018-09-27 14:23:32 +0200

    #8 Remove customfilter after view is rendered to fix multiple input fields.

--- view/view_class.php
+++ view/view_class.php
@@ -687,6 +687,12 @@ abstract class datalynxview_base {
             $this->view->{"e$editor"} = $this->unmask_tags($text);
             $this->view->{"e$editor"} = str_replace($tags, $replacements, $this->view->{"e$editor"});
         }
+        // Remove customfilter tags after we have displayed them.
+        foreach ($tags as $key=>$value) {
+            if (strpos($value, '##customfilter') !== false) {
+                unset($this->_tags['view'][$key]);
+            }
+        }
     }
 
     /**
