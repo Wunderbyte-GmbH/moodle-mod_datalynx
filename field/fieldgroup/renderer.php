bc6a475d65afa2f4b3e68b1b4a4301012a77167e
michaelpollak <github@michaelpollak.org>
2018-11-23 13:28:19 +0100

    Found existing renderer to fix missing constructor.

--- field/fieldgroup/renderer.php
+++ field/fieldgroup/renderer.php
@@ -40,42 +40,18 @@ class datalynxfield_fieldgroup_renderer extends datalynxfield_renderer {
         // Loop through showdefault.
         $showdefault = $this->_field->field->param3;
         for ($x = 0; $x < $showdefault; $x++) {
-            $displ .= "</td></tr><tr><td>";
+            $displ .= "</td></tr><tr><td>"; // TODO: How to get this in a template? Close current template and start new.
 
             foreach ($fieldgroupfields as $fieldname) {
-                $subfield = clone $this->_field; // Clone fields so we don't mess with references.
-                $subfield->field = $this->get_fieldgroup_from_name($fieldname); // Attach subfield.
+                $subfieldid = $this->get_fieldid_from_name($fieldname);
+                $subfield = $this->_field->df->get_field_from_id($subfieldid);
 
-                // E.g. for urls we need specific properties, how to fix this for all types?
-                $params = $this->fieldspecific($subfield, $params);
-
-                // TODO: Test with all field classes..
-                $rendererclass = "datalynxfield_{$subfield->field->type}_renderer";
-                require_once("$CFG->dirroot/mod/datalynx/field/{$subfield->field->type}/renderer.php");
-                $fieldclass = new $rendererclass($subfield);
-
-                // Retrieve only relevant part of content and hand it over.
-                // TODO: This throws errors when _fieldgroup is not set.
-                if (isset($entry->{"c{$subfield->field->id}_content_fieldgroup"})) {
-                    $contentarray = $entry->{"c{$subfield->field->id}_content_fieldgroup"};
-                } else {
-                    // If we have exactly one content, show this and leave the rest blank.
-                    if (isset( $entry->{"c{$subfield->field->id}_content"} )) {
-                        $contentarray = array( $entry->{"c{$subfield->field->id}_content"} );
-                    } else {
-                        $contentarray = array();
-                    }
-                }
-
-                if (isset($contentarray[$x])) {
-                    $entry->{"c{$subfield->field->id}_content"} = $contentarray[$x];
-                } else {
-                    $entry->{"c{$subfield->field->id}_content"} = "";
-                }
+                $this->splitcontent($entry, $subfieldid, $x);
 
                 $displ .= "" . $subfield->field->name . ": "; // Needs to be automated here, no html.
-                $displ .= $fieldclass->render_display_mode($entry, $params);
+                $displ .= $subfield->renderer()->render_display_mode($entry, $params);
                 $displ .= "     ";
+
             }
         }
         return $displ;
@@ -93,38 +69,17 @@ class datalynxfield_fieldgroup_renderer extends datalynxfield_renderer {
             $mform->addElement('html', '</td></tr><tr><td>'); // Fix this table thing. TODO: Get rid of this table and use css.
 
             foreach ($fieldgroupfields as $fieldname) {
-                $subfield = clone $this->_field; // Clone fields so we don't mess with references.
-                $subfield->field = $this->get_fieldgroup_from_name($fieldname); // Attach subfield.
+                $subfieldid = $this->get_fieldid_from_name($fieldname);
+                $subfield = $this->_field->df->get_field_from_id($subfieldid);
 
-                // TODO: Test with all field classes..
-                $rendererclass = "datalynxfield_{$subfield->field->type}_renderer";
-                require_once("$CFG->dirroot/mod/datalynx/field/{$subfield->field->type}/renderer.php");
-                $fieldclass = new $rendererclass($subfield);
-
-                // Retrieve only relevant part of content and hand it over.
-                if ( isset ( $entry->{"c{$subfield->field->id}_content_fieldgroup"}) ) {
-                    $tempcontent = $entry->{"c{$subfield->field->id}_content_fieldgroup"};
-                } else {
-                    // If we have exactly one content, show this and leave the rest blank.
-                    if (isset( $entry->{"c{$subfield->field->id}_content"} )) {
-                        $tempcontent = array( $entry->{"c{$subfield->field->id}_content"} );
-                    } else {
-                        $tempcontent = array();
-                    }
-                }
-                $tempentryid = $entry->id;
-
-                // Don't touch content if it is not a fieldgroup.
-                if (isset($tempcontent[$x])) {
-                    $entry->{"c{$subfield->field->id}_content"} = $tempcontent[$x];
-                } else {
-                    $entry->{"c{$subfield->field->id}_content"} = "";
-                }
+                $this->splitcontent($entry, $subfieldid, $x);
 
                 // Add a static label.
                 $mform->addElement('static', '', $subfield->field->name . ": ");
+                $tempentryid = $entry->id;
                 $entry->id = $entry->id . "_" . $x; // Add iterator to fieldname.
-                $fieldclass->render_edit_mode($mform, $entry, $options);
+                $subfield->renderer()->render_edit_mode($mform, $entry, $options);
+
 
                 // Restore entryid to prior state.
                 $entry->id = $tempentryid;
@@ -154,23 +109,31 @@ class datalynxfield_fieldgroup_renderer extends datalynxfield_renderer {
         return $patterns;
     }
 
-    public static function get_fieldgroup_from_name($name) {
+    public static function get_fieldid_from_name($name) {
         global $DB;
         $record = $DB->get_record('datalynx_fields', array('name' => $name));
-        return $record;
+        return $record->id;
     }
 
-    public static function fieldspecific($subfield, $params) {
-        switch ($subfield->field->type) {
-            case 'url':
-                // Urls call property class and target.
-                $subfield->class = $subfield->field->param3;
-                $subfield->target = $subfield->field->param4;
-                // Urls do use a parameter here.
-                $params['link'] = true;
-                break;
+    public static function splitcontent($entry, $subfieldid, $iterator) {
+        // Retrieve only relevant part of content and hand it over.
+        if ( isset ( $entry->{"c{$subfieldid}_content_fieldgroup"}) ) {
+            $tempcontent = $entry->{"c{$subfieldid}_content_fieldgroup"};
+        } else {
+            // If we have exactly one content, show this and leave the rest blank.
+            if (isset( $entry->{"c{$subfieldid}_content"} )) {
+                $tempcontent = array( $entry->{"c{$subfieldid}_content"} );
+            } else {
+                $tempcontent = array();
+            }
         }
-        // Return the possible unchanged params.
-        return $params;
+
+        // Don't touch content if it is not a fieldgroup.
+        if (isset($tempcontent[$iterator])) {
+            $entry->{"c{$subfieldid}_content"} = $tempcontent[$iterator];
+        } else {
+            $entry->{"c{$subfieldid}_content"} = "";
+        }
     }
+
 }
