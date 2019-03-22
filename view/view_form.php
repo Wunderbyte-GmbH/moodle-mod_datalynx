1f41e8bbce5aae160997675edb57c49019cfbf0d
michaelpollak <github@michaelpollak.org>
2019-03-22 15:07:54 +0100

    Rollbeack to test if behat is happy now.

--- view/view_form.php
+++ view/view_form.php
@@ -276,47 +276,6 @@ class datalynxview_base_form extends moodleform {
             $errors['name'] = get_string('invalidname', 'datalynx', get_string('view', 'datalynx'));
         }
 
-        // Check if a field is used multiple times in entryview.
-        $entryview = $data['eparam2_editor']['text'];
-
-        // We check if fieldgroups is used multiple times or if subfields are repeated.
-        foreach ($view->field_tags()['Fieldgroups']['Fieldgroups'] as $fieldgroup) {
-
-            // Stop if the fieldgroup is not used in this entryview.
-            if (strpos($entryview, $fieldgroup) === false) {
-                continue;
-            }
-
-            $fieldid = array_search(substr($fieldgroup, 2, -2), $df->get_fieldnames());
-            $subfields = $df->get_field_from_id($fieldid);
-
-            $lookup = '';
-            foreach ($subfields->fieldids as $subfieldid) {
-                $subfield = $df->get_field_from_id($subfieldid);
-                $lookup .= " [[".$subfield->field->name."]]";
-            }
-
-            // Find in view and append tags for individual fields.
-            $entryview = str_replace($fieldgroup, $lookup, $entryview);
-        }
-
-        foreach ($view->field_tags()['Fields']['Fields'] as $field) {
-
-            // $field = substr($field, 0, -2); // Remove end brackets to also trigger with different renderers [[aa and [[aa:b .
-
-            // Error when we find more than one instance of this tag.
-            if (substr_count($entryview, $field) > 1 ) {
-
-                // Make sure multiple errors are shown.
-                if (!array_key_exists('eparam2_editor', $errors)) {
-                    $errors['eparam2_editor'] = get_string('viewrepeatedfields', 'datalynx', substr($field, 2, -2));
-                } else {
-                    $errors['eparam2_editor'] .= "<br>" . get_string('viewrepeatedfields', 'datalynx', substr($field, 2, -2));
-                }
-
-            }
-        }
-
         return $errors;
     }
 }
