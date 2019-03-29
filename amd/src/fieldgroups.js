750b6a76ff2ee0eab94499e19b8d03784090a551
michaelpollak <github@michaelpollak.org>
2019-03-29 11:59:13 +0100

    #22 Get rid of ids and use data-line instead.

--- amd/src/fieldgroups.js
+++ amd/src/fieldgroups.js
@@ -2,35 +2,35 @@ define(["jquery"], function($) {
 
     return {
 
-        init: function(defaultlines, maxlines) {
+        init: function(fieldgroupname, defaultlines, maxlines) {
 
             // We hide lines after the last line we show by default.
             defaultlines++;
 
             // Loop from defaultlines to maxlines.
             for (var line = defaultlines; line <= maxlines; line++) {
-                $("#mform1 #line_" + line).hide(); // Hide the whole line.
+                $("div[data-field-name='" + fieldgroupname + "'] [data-line='" + line + "']").hide(); // Hide the whole line.
             }
 
             // Add button functionality.
-            $( "#id_addline" ).each(function () {
+            $("div[data-field-name='" + fieldgroupname + "'] #id_addline").each(function () {
                     $(this).off( "click" );
                     $(this).click(function(e) {
                         e.preventDefault(); // Don't follow hrefs.
-                        if ($('input[name=visiblelines]').get(0).value < maxlines) {
-                            $('.lines:hidden:first').show(); // Find the first id that is hidden.
-                            $('input[name=visiblelines]').get(0).value++; // Add one to the visible lines input.
+                        if ($("input[name=visiblelines]").get(0).value < maxlines) {
+                            $("div[data-field-name='" + fieldgroupname + "'] .lines:hidden:first").show(); // Find the first hidden.
+                            $("input[name=visiblelines]").get(0).value++; // Add one to the visible lines input.
                         }
                     });
             });
 
-            $( "#id_hideline" ).each(function () {
+            $("div[data-field-name='" + fieldgroupname + "'] #id_hideline").each(function () {
                     $(this).off( "click" );
                     $(this).click(function(e) {
                         e.preventDefault(); // Don't follow hrefs.
-                        if ($('input[name=visiblelines]').get(0).value > 0) {
-                            $('.lines:visible:last').hide();
-                            $('input[name=visiblelines]').get(0).value--;
+                        if ($("input[name=visiblelines]").get(0).value > 0) {
+                            $("div[data-field-name='" + fieldgroupname + "'] .lines:visible:last").hide();
+                            $("input[name=visiblelines]").get(0).value--;
                         }
                     });
             });
