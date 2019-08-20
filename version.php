a7730aefae9c8833fac3ee1d35adfda32ff6feb0
David Bogner <davidbogner@gmail.com>
2019-08-20 11:04:46 +0200

    Release v1.7 Data Science

--- version.php
+++ version.php
@@ -22,9 +22,9 @@
  */
 defined('MOODLE_INTERNAL') or die();
 $plugin->component = 'mod_datalynx';
-$plugin->version = 2019060400;
-$plugin->release = '1.6.1 Kim Wexler'; // Famous movie, theater and tv series characters.
-$plugin->requires = 2016052300;
+$plugin->version = 2019082000;
+$plugin->release = '1.7 Data Science'; // Data words like data science, data mining.
+$plugin->requires = 2018051700;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = array(
     'assignfeedback_editpdf' => ANY_VERSION // We use fpdi from this core module.
