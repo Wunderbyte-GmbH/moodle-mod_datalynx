8deb72393fa9cfe7effa4f7dd5bd52f7b9b852d7
michaelpollak <github@michaelpollak.org>
2019-09-04 11:53:11 +0200

    Fixing array keys to start with 1 to be compatible with fielddef #33

--- field/multiselect/field_class.php
+++ field/multiselect/field_class.php
@@ -106,6 +106,10 @@ class datalynxfield_multiselect extends datalynxfield_option_multiple {
 
         // Check if all values are known in field definition.
         $knownvalues = explode("\n", $this->field->param1);
+
+        // Our values start counting at 1, correct knownvalues array keys.
+        $knownvalues = array_combine(range(1, count($knownvalues)), $knownvalues);
+        
         $addoption = null;
         foreach ($values[''] as $key => $value) {
             if (array_key_exists($value, $knownvalues)) {
