5a9ecd68405e2d9f6e5113b4ae4a13b57573d20e
michaelpollak <github@michaelpollak.org>
2019-09-18 13:07:05 +0200

    :art:

--- field/checkbox/field_form.php
+++ field/checkbox/field_form.php
@@ -30,7 +30,7 @@ class datalynxfield_checkbox_form extends datalynxfield_multiselect_form {
 
     public function field_definition() {
         parent::field_definition();
-        
+
         $mform = &$this->_form;
 
         // Hide autocomplete, it does not apply.
