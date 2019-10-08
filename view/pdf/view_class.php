1821c9bfb8da3ab46f948bb8c8d5cc3837c32f39
michaelpollak <github@michaelpollak.org>
2019-10-08 12:21:57 +0200

    Use ghostscript to fix the pdf version issue, not working here yet. #73

--- view/pdf/view_class.php
+++ view/pdf/view_class.php
@@ -779,6 +779,8 @@ class datalynxview_pdf extends datalynxview_base {
      * Current code assumes we want to attach all files linked to this context.
      */
     protected function mergepdfs($pdf, $pagecount) {
+        global $CFG;
+
         // Check what fields are file class.
         $filefieldids = array();
         foreach ($this->get_view_fields() as $fieldid => $fieldinview) {
@@ -831,7 +833,25 @@ class datalynxview_pdf extends datalynxview_base {
             if ($file->copy_content_to($filepath)) {
                 $this->_tmpfiles[] = $filepath;
 
-                $importpagecount = $pdf->setSourceFile($filepath);
+                $importpagecount = 0;
+                // Try if this pdf files version is <= 1.4 to work with pdftk.
+                try {
+                    $importpagecount = $pdf->setSourceFile($filepath);
+                } catch (\Exception $e) {
+                    // PDF was not valid - try running it through ghostscript to clean it up.
+                    $gsexec = \escapeshellarg($CFG->pathtogs);
+                    $filepath = \escapeshellarg($filepath);
+                    $arguments = " -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dBATCH -dNOPAUSE"; // Add -q, remove 2>&1!
+                    shell_exec("$gsexec $arguments -sOutputFile=$filepath $filepath 2>&1");
+                }
+
+                // Try once more, if not just skip this file.
+                try {
+                    $importpagecount = $pdf->setSourceFile($filepath);
+                } catch (\Exception $e) {
+                    return $pagecount;
+                }
+
                 for ($pagenumber = 1; $pagenumber <= $importpagecount; $pagenumber++) {
                     $importtemplate = $pdf->ImportPage($pagenumber);
                     $pdf->AddPage();
