3f5c6b608c2232a32d572c2898c019b7ca53bab0
michaelpollak <github@michaelpollak.org>
2020-02-17 10:41:15 +0100

    Somehow this does not take attributes well, needs checking #98

--- field/field_form.php
+++ field/field_form.php
@@ -124,9 +124,9 @@ class datalynxfield_option_form extends datalynxfield_form {
             foreach ($options as $id => $option) {
                 $group[] = &$mform->createElement('static', null, null,
                         "<tr><td>{$option}</td><td>");
-                $group[] = &$mform->createElement('text', "renameoption[{$id}]", '', array('size' => 32));
+                $group[] = &$mform->createElement('text', "renameoption[{$id}]", '');
                 $group[] = &$mform->createElement('static', null, null, '</td><td>');
-                $group[] = &$mform->createElement('checkbox', "deleteoption[{$id}]", '', array('size' => 1));
+                $group[] = &$mform->createElement('checkbox', "deleteoption[{$id}]", '');
                 foreach ($options as $newid => $newoption) {
                     $mform->disabledIf("renameoption[{$id}]", "deleteoption[{$newid}]", 'checked');
                 }
